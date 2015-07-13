ea9cd7bfaca277c3dcd07871e14943821cb4c0d5
Florian Simon <github@verm-luh.de>
2015-07-13 11:39:11 +0200

    Update relay_state.rb
    working version with automatic state switching

--- relay_state.rb
+++ relay_state.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+# encoding: UTF-8
+
 require 'net/http'
 require 'uri'
 require 'awesome_print'
@@ -7,23 +9,23 @@ require 'json/ext'
 
 uri = URI.parse('http://192.168.4.1/relay')
 
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Get.new(uri.request_uri)
-request.basic_auth 'olimex', 'olimex'
-response = http.request(request)
-json_response =  JSON.parse response.body
-ap json_response
-ap json_response['Data']
-puts json_response.to_json
+Net::HTTP.new(uri.host, uri.port, nil, nil, nil, nil).start do |client|
+  puts 'current relay state...'
+  get_request = Net::HTTP::Get.new uri.request_uri
+  get_request.basic_auth 'olimex', 'olimex'
+  response = client.request get_request
+  get_response =  JSON.parse response.body
+  ap get_response
 
+  relay_state = get_response['Data']['Relay']
+  new_relay_state = (relay_state + 1) % 2
 
-post_request = Net::HTTP::Post.new(uri.request_uri)
-post_request.body = '{"Relay" : 0}'
-post_request.basic_auth 'olimex', 'olimex'
-post_request['content-type'] = 'text/plain'
-ap post_request
-ap post_request.body
-response = http.request(post_request)
-json_response = JSON.parse response.body
-ap json_response.to_json
-
+  puts 'setting new relay state...'
+  post_request = Net::HTTP::Post.new uri.request_uri
+  post_request.basic_auth 'olimex', 'olimex'
+  post_request['content-type'] = 'text/plain;charset=utf-8'
+  post_request.body = %*{"Relay" : #{new_relay_state}}*
+  response = client.request post_request
+  post_response = JSON.parse response.body
+  ap post_response
+end
